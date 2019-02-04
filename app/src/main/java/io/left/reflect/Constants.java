62a611e5b797bf1227f96543afeaf0d983e43543
peter <peter@rightmesh.io>
2019-02-04 11:11:08 -0800

    Revert `MESH_PORT` value

--- app/src/main/java/io/left/reflect/Constants.java
+++ app/src/main/java/io/left/reflect/Constants.java
@@ -5,5 +5,5 @@ public class Constants {
      * MESH_PORT is the mesh port that this app is allowed to run on, according to your license key.
      * See developer.rightmesh.io for more details.
      */
-    public static final int MESH_PORT = 1001;
+    public static final int MESH_PORT = 9876;
 }
